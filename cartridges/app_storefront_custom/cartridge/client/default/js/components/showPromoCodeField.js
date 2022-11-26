1d28d1f4d30346480483d59a050fd8ff6369dd7c
Vlad Avram <55098337+VladAvram232323@users.noreply.github.com>
2022-11-26 16:48:12 +0200

    2-Using-CSS-to-hide/show

--- cartridges/app_storefront_custom/cartridge/client/default/js/components/showPromoCodeField.js
+++ cartridges/app_storefront_custom/cartridge/client/default/js/components/showPromoCodeField.js
@@ -1,5 +1,12 @@
+// Uncomment if you want to use the js version instead of CSS one
+
+
+/*
+
 $("#promo-code-link").on("click", (e) => {
     e.preventDefault();
     $("#promo-code-wrapper").toggleClass("hidden").addClass("show");
     $("#promo-code-link").toggleClass("show").addClass("hidden");
 });
+
+*/
